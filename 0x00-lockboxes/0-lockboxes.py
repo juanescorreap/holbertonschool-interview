0984cd70ebe93263c2e3a407c9dfd95f31f7d324
juanescorreap <juanestebancope@outlook.com>
2022-04-19 17:56:53 -0500

    method that determines id all boxes can be opened

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Method that determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
