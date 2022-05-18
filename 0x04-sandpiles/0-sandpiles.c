dda12dc1a4a2c2088dc934f8898f1fc1f4469a66
juanescorreap <juanestebancope@outlook.com>
2022-05-17 20:03:35 -0500

    ugh

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -10,6 +10,7 @@ static void print_grid(int grid[3][3])
 {
     int i, j;
 
+    printf("=\n");
     for (i = 0; i < 3; i++)
     {
         for (j = 0; j < 3; j++)
@@ -23,15 +24,14 @@ static void print_grid(int grid[3][3])
 }
 
 /**
- * sandpiles_sum - Function that computes the sum of two sandpiles
+ * sandpiles_addition - Function that computes the sum of two sandpiles
  * @grid1: Left 3x3 grid
  * @grid2: Right 3x3 grid
  *
  */
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+void sandpiles_addition(int grid1[3][3], int grid2[3][3])
 {
     int i, j;
-    int processAgain = 1;
 
     for (i = 0; i < 3; i++)
     {
@@ -40,57 +40,83 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
             grid1[i][j] = grid1[i][j] + grid2[i][j];
         }
     }
-    while (processAgain == 1)
+}
+/**
+ * sandpiles_topple - Function that topples a sandpile
+ * @grid1: 3x3 grid
+ *
+ */
+void sandpiles_topple(int grid1[3][3])
+{
+    int i, j;
+    int gridtmp[3][3];
+
+    for (i = 0; i < 3; i++)
     {
-        processAgain = 0;
-
-        for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
         {
-            for (j = 0; j < 3; j++)
+            gridtmp[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid1[i][j] >= 4)
             {
-                if (grid1[i][j] >= 4)
+                if (i - 1 >= 0)
                 {
-                    if (i - 1 >= 0)
-                    {
-                        grid1[i - 1][j] += 1;
-                        if (grid1[i - 1][j] >= 4)
-                        {
-                            processAgain = 1;
-                        }
-                    }
-                    if (i + 1 < 3)
-                    {
-                        grid1[i + 1][j] += 1;
-                        if (grid1[i + 1][j] >= 4)
-                        {
-                            processAgain = 1;
-                        }
-                    }
-                    if (j - 1 >= 0)
-                    {
-                        grid1[i][j - 1] += 1;
-                        if (grid1[i][j - 1] >= 4)
-                        {
-                            processAgain = 1;
-                        }
-                    }
-                    if (j + 1 < 3)
-                    {
-                        grid1[i][j + 1] += 1;
-                        if (grid1[i][j + 1] >= 4)
-                        {
-                            processAgain = 1;
-                        }
-                    }
-                    grid1[i][j] -= 4;
-                    if (grid1[i][j] >= 4)
-                    {
-                        processAgain = 1;
-                    }
-
-
+                    gridtmp[i - 1][j] += 1;
                 }
+                if (i + 1 < 3)
+                {
+                    gridtmp[i + 1][j] += 1;
+                }
+                if (j - 1 >= 0)
+                {
+                    gridtmp[i][j - 1] += 1;
+                }
+                if (j + 1 < 3)
+                {
+                    gridtmp[i][j + 1] += 1;
+                }
+                grid1[i][j] -= 4;
             }
         }
+    }
+    sandpiles_addition(grid1, gridtmp);
+}
+/**
+ * when_to_print - Function that indicates when to print a sandpile
+ * @grid1: 3x3 grid
+ *
+ */
+int when_to_print(int grid1[3][3])
+{
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid1[i][j] >= 4)
+                return (0);
+        }
+    }
+    return (1);
+}
+/**
+ * sandpiles_addition - Function that computes the sum and toppling of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    sandpiles_addition(grid1, grid2);
+    while(!when_to_print(grid1))
+    {
+        print_grid(grid1);
+        sandpiles_topple(grid1);
     }
 }
