4bc5dd910dce6b3f65ffda69644971904ab74202
juanescorreap <juanestebancope@outlook.com>
2022-05-18 18:38:48 -0500

    tryint to find the missing requirements

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,10 +2,10 @@
 #define SANDPILES_H
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void grid_print(int grid[3][3]);
 void sandpiles_addition(int grid1[3][3], int grid2[3][3]);
 void sandpiles_topple(int grid1[3][3]);
 int when_to_print(int grid1[3][3]);
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
+
 
 #endif /* SANDPILES_H */
