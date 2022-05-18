f8a20fb57d1e0bfd1dfca0264987525e133f9464
juanescorreap <juanestebancope@outlook.com>
2022-05-18 15:03:43 -0500

    blank lines

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,9 +1,11 @@
 #ifndef _SANDPILES_H_
 #define _SANDPILES_H_
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 void sandpiles_addition(int grid1[3][3], int grid2[3][3]);
 void sandpiles_topple(int grid1[3][3]);
 int when_to_print(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
 #endif /*_SANDPILES_H_*/
