0fee016dd05c8fd9bf5cf2c98b0a570c38b4a9f9
juanescorreap <juanestebancope@outlook.com>
2022-06-01 13:39:56 -0500

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,31 +5,41 @@ Script that reads stdin line by line and computes metrics
 import sys
 
 
-def print_status(size, status):
-    '''
-    Print the status
-    '''
-    print('File size: {}'.format(size))
-    for key, value in sorted(status.items()):
-        if value:
+i = 0
+sum_file_size = 0
+status_code = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
+
+try:
+    for line in sys.stdin:
+        args = line.split(' ')
+        if len(args) > 2:
+            status_line = args[-2]
+            file_size = args[-1]
+            if status_line in status_code:
+                status_code[status_line] += 1
+            sum_file_size += int(file_size)
+            i += 1
+            if i == 10:
+                print('File size: {:d}'.format(sum_file_size))
+                sorted_keys = sorted(status_code.keys())
+                for key in sorted_keys:
+                    value = status_code[key]
+                    if value != 0:
+                        print('{}: {}'.format(key, value))
+                i = 0
+except Exception:
+    pass
+finally:
+    print('File size: {:d}'.format(sum_file_size))
+    sorted_keys = sorted(status_code.keys())
+    for key in sorted_keys:
+        value = status_code[key]
+        if value != 0:
             print('{}: {}'.format(key, value))
-
-
-if __name__ == '__main__':
-    size, count = 0, 0
-    status = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
-    try:
-        for line in sys.stdin:
-            args = line.split()
-            if len(args) > 2:
-                if args[-2] in status:
-                    status[args[-2]] += 1
-                size += int(args[-1])
-            count += 1
-            if not count % 10:
-                print_status(size, status)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print_status(size, status)
