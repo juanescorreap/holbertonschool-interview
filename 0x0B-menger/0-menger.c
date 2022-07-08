4133548aeda5b92eb5e5dcbf7afa36e5edad91d8
juanescorreap <juanestebancope@outlook.com>
2022-07-07 19:24:50 -0500

    task 0

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,39 @@
+#include "menger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+/**
+ * menger - Function that draws a 2D Menger Sponge
+ * @level:  Level of the Menger Sponge to draw
+ * Return: void
+ */
+void menger(int level)
+{
+    int col, row, size;
+
+    if (level >= 0)
+    {
+        size = pow(3, level);
+        for (col = 0; col < size; col++)
+        {
+            for (row = 0; row < size; row++)
+                printf("%c", find_char(col, row));
+            printf("\n");
+        }
+    }
+}
+/**
+ * printer - Checks wheter a "#" or a " "
+ * corresponds to each coordinate point
+ * @col: Column
+ * @row: Row
+ * Return: # or " "
+ */
+char printer(int col, int row)
+{
+    for (; col && row; col /= 3, row /= 3)
+        if (col % 3 == 1 && row % 3 == 1)
+            return (' ');
+    return ('#');
+}
---
+++ 0x0B-menger/README.md
@@ -0,0 +1,5 @@
+# 0x0B. Menger sponge
+
+Project developed by Juan Esteban Correa Pérez as part of the Web Stack specialization program at Holberton School.
+
+It's purpose is to help the student prepare for job interviews.
