f75c341773a072f400db98bdbcde910e0bb70c28
juanescorreap <juanestebancope@outlook.com>
2022-07-21 18:24:05 -0500

    N Queens problem

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,67 +3,28 @@
 The N queens puzzle is the challenge of placing
 N non-attacking queens on an NxN chessboard
 """
-global N
-N = 4
+import sys
 
 
-def printSolution(board):
-    for i in range(N):
-        for j in range(N):
-            print(board[i][j], end=" ")
-        print()
+def nqueens(n: int):
+    """
+    backtracking
+    """
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
 
 
-def isSafe(board, row, col):
+if __name__ == "__main__":
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("Usage: nqueens N")
+        exit(1)
 
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
 
-    for i, j in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
 
-    for i, j in zip(range(row, N, 1),
-                    range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solveNQUtil(board, col):
-
-    if col >= N:
-        return True
-
-    for i in range(N):
-
-        if isSafe(board, i, col):
-
-            board[i][col] = 1
-
-            if solveNQUtil(board, col + 1) is True:
-                return True
-
-            board[i][col] = 0
-
-    return False
-
-
-def solveNQ():
-    board = [[0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, 0]]
-
-    if solveNQUtil(board, 0) is False:
-        print("Solution does not exist")
-        return False
-
-    printSolution(board)
-    return True
-
-
-solveNQ()
+    nqueens(int(sys.argv[1]))
