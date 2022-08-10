8857aefac08165625b59e1dc4475114286612027
juanescorreap <juanestebancope@outlook.com>
2022-08-09 19:02:55 -0500

    README.md and task

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,13 +5,13 @@ const request = require('request');
 const url = 'https://swapi.dev/api/films/'
 const movieId = process.argv[2];
 
-request(url+movieId, (error, response, body) => {
+request(url + movieId, (error, response, body) => {
     if (error) {
         throw error;
     } else {
         const characters = JSON.parse(body).characters;
         for (const item of characters) {
-            const response1 = await new Promise((resolve, reject)=> {
+            const response1 = await new Promise((resolve, reject) => {
                 request(item, (error, response1, body1) => {
                     if (error) {
                         reject(error);
