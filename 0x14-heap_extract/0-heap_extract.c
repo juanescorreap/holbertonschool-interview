fcbbb23c10f53b59d667c9ab2eec9ad885e77c0c
juanescorreap <juanestebancope@outlook.com>
2022-09-15 12:18:54 -0500

    heap extract

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,99 +1,127 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "binary_trees.h"
-
-/* Original code from http://stackoverflow.com/a/13755911/5184480 */
-
 /**
- * print_t - Stores recursively each level in an array of strings
- *
- * @tree: Pointer to the node to print
- * @offset: Offset to print
- * @depth: Depth of the node
- * @s: Buffer
- *
- * Return: length of printed tree after process
+ * heapify - Function that fix the heap max
+ * @root: pointer to the head of the heap tree
+ * Return: void function
  */
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+void heapify(binary_tree_t *root)
 {
-	char b[6];
-	int width, left, right, is_left, i;
+	binary_tree_t *child = NULL;
+	int num = 0;
 
-	if (!tree)
-		return (0);
-	is_left = (tree->parent && tree->parent->left == tree);
-	width = sprintf(b, "(%03d)", tree->n);
-	left = print_t(tree->left, offset, depth + 1, s);
-	right = print_t(tree->right, offset + left + width, depth + 1, s);
-	for (i = 0; i < width; i++)
-		s[depth][offset + left + i] = b[i];
-	if (depth && is_left)
+	while (1)
 	{
-		for (i = 0; i < width + right; i++)
-			s[depth - 1][offset + left + width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	else if (depth && !is_left)
-	{
-		for (i = 0; i < left + width; i++)
-			s[depth - 1][offset - width / 2 + i] = '-';
-		s[depth - 1][offset + left + width / 2] = '.';
-	}
-	return (left + width + right);
-}
-
-/**
- * _height - Measures the height of a binary tree
- *
- * @tree: Pointer to the node to measures the height
- *
- * Return: The height of the tree starting at @node
- */
-static size_t _height(const binary_tree_t *tree)
-{
-	size_t height_l;
-	size_t height_r;
-
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
-}
-
-/**
- * binary_tree_print - Prints a binary tree
- *
- * @tree: Pointer to the root node of the tree to print
- */
-void binary_tree_print(const binary_tree_t *tree)
-{
-	char **s;
-	size_t height, i, j;
-
-	if (!tree)
-		return;
-	height = _height(tree);
-	s = malloc(sizeof(*s) * (height + 1));
-	if (!s)
-		return;
-	for (i = 0; i < height + 1; i++)
-	{
-		s[i] = malloc(sizeof(**s) * 255);
-		if (!s[i])
-			return;
-		memset(s[i], 32, 255);
-	}
-	print_t(tree, 0, 0, s);
-	for (i = 0; i < height + 1; i++)
-	{
-		for (j = 254; j > 1; --j)
+		if (!root->left)
+			break;
+		else if (!root->right)
+			child = root->left;
+		else
 		{
-			if (s[i][j] != ' ')
-				break;
-			s[i][j] = '\0';
+			if (root->left->n >= root->right->n)
+				child = root->left;
+			else
+				child = root->right;
 		}
-		printf("%s\n", s[i]);
-		free(s[i]);
+
+		if (root->n >= child->n)
+			break;
+
+		num = root->n;
+		root->n = child->n;
+		child->n = num;
+
+		root = child;
 	}
-	free(s);
+}
+
+/**
+ * num_nodes - Function that stores the number of nodes of heap tree
+ * @root: Pointer to the head of the heap tree
+ * Return: number of nodes in heap tree
+ */
+int num_nodes(heap_t *root)
+{
+	int l = 0, r = 0;
+
+	if (!root)
+		return (0);
+
+	l = num_nodes(root->left);
+	r = num_nodes(root->right);
+
+	return (1 + l + r);
+}
+
+/**
+ * last_node_func - Function that gets the last node in heap min
+ * @root: pointer to the head of the heap tree
+ * Return: The last node in heap min
+ */
+binary_tree_t *last_node_func(heap_t *root)
+{
+	int nodes = 0, size_heap = 0;
+	binary_tree_t *last_node = NULL;
+
+	size_heap = num_nodes(root);
+
+	for (nodes = 1; nodes <= size_heap; nodes <<= 1)
+		;
+	nodes >>= 2;
+
+	for (last_node = root; nodes > 0; nodes >>= 1)
+	{
+		if (size_heap & nodes)
+			last_node = last_node->right;
+		else
+			last_node = last_node->left;
+	}
+
+	return (last_node);
+}
+/**
+ * heap_extract - Function that extracts the root node of a Max Binary Heap.
+ * @root: Double pointer to the root node of the heap.
+ * Return: Value stored in the root node, or 0 if fail.
+ */
+int heap_extract(heap_t **root)
+{
+	binary_tree_t *last_node = NULL, *head_node = NULL;
+	int extracted_num = 0;
+
+	if (!root || !*root)
+		return (0);
+	head_node = *root;
+	if (!head_node->left && !head_node->right)
+	{
+		extracted_num = head_node->n;
+		free(head_node);
+		*root = NULL;
+		return (extracted_num);
+	}
+
+	head_node = *root;
+	extracted_num = head_node->n;
+
+	last_node = last_node_func(*root);
+
+	if (last_node->parent->left == last_node)
+		last_node->parent->left = NULL;
+	else
+		last_node->parent->right = NULL;
+
+	last_node->left = head_node->left;
+	last_node->right = head_node->right;
+	last_node->parent = head_node->parent;
+
+	if (head_node->left)
+		head_node->left->parent = last_node;
+	if (head_node->right)
+		head_node->right->parent = last_node;
+
+	*root = last_node;
+	free(head_node);
+
+	heapify(*root);
+
+	return (extracted_num);
 }
