e8c0c5a618b928521600fd2515fa4742e8f164c6
juanescorreap <juanestebancope@outlook.com>
2022-12-15 19:40:48 -0500

    regex

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -7,22 +7,22 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-    int dot = 0;
-    int asterisk = 0;
+	int dot = 0;
+	int asterisk = 0;
 
-    if (!str || !pattern)
-        return (0);
+	if (!str || !pattern)
+		return (0);
 
-    dot = *str && (*str == *pattern || *pattern == '.');
-    asterisk = *(pattern + 1) == '*';
+	dot = *str && (*str == *pattern || *pattern == '.');
+	asterisk = *(pattern + 1) == '*';
 
-    if (!*str && !asterisk)
-        return (*pattern ? 0 : 1);
-    else if (dot && asterisk)
-        return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
-    else if (dot && !asterisk)
-        return (regex_match(str + 1, pattern + 1));
-    else if (asterisk)
-        return (regex_match(str, pattern + 2));
-    return (0);
+	if (!*str && !asterisk)
+		return (*pattern ? 0 : 1);
+	else if (dot && asterisk)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	else if (dot && !asterisk)
+		return (regex_match(str + 1, pattern + 1));
+	else if (asterisk)
+		return (regex_match(str, pattern + 2));
+	return (0);
 }
